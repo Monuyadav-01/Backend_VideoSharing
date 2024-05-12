5ad7a1bb8724fa387ba5c2cb59742e207ebb1d46
monu <monuyadav10012000@gmail.com>
2024-05-13 00:28:57 +0530

    connectdatabase

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const dbconnection = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(`host ${dbconnection.connection.host}`);
+  } catch (error) {
+    console.log(`Error ${error}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
